dba5615190b60657015c10241c82b5b592236471
SantiagoSanchezV <santisanchez21456@gmail.com>
2025-10-23 10:39:27 -0500

    Modificaciones para funcionamiento de MySQL

--- Proyecto_GC/models.py
+++ Proyecto_GC/models.py
@@ -1,27 +1,17 @@
 from flask_login import UserMixin
+from app import mysql
 
-# ------------------ Clase User ------------------
 class User(UserMixin):
     def __init__(self, id, username, password):
-        self.id = str(id)  # Flask-Login espera string
+        self.id = id
         self.username = username
         self.password = password
 
-# ------------------ Funciones auxiliares ------------------
-def get_user_by_id(db_connection, user_id):
-    cur = db_connection.cursor()
+def load_user(user_id):
+    cur = mysql.connection.cursor()
     cur.execute("SELECT id, username, password FROM users WHERE id = %s", (user_id,))
-    user_data = cur.fetchone()
+    user = cur.fetchone()
     cur.close()
-    if user_data:
-        return User(user_data[0], user_data[1], user_data[2])
-    return None
-
-def get_user_by_username(db_connection, username):
-    cur = db_connection.cursor()
-    cur.execute("SELECT id, username, password FROM users WHERE username = %s", (username,))
-    user_data = cur.fetchone()
-    cur.close()
-    if user_data:
-        return User(user_data[0], user_data[1], user_data[2])
+    if user:
+        return User(int(user[0]), user[1], user[2])
     return None
